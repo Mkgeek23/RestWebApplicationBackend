07335233afb2c752e21b611b97030cea0e36ff64
Mkgeek23 <47983765+Mkgeek23@users.noreply.github.com>
2022-12-14 13:45:29 +0100

    Init

---
+++ src/main/java/pl/emkgeek/restwebapplicationbackend/RestWebApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.emkgeek.restwebapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/emkgeek/restwebapplicationbackend/RestWebApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.emkgeek.restwebapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
